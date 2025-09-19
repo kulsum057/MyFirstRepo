9fb9180e1bced769d5c94f209e593317eafb426e
kulsum057 <bibikulsumprova@gmail.com>
2025-09-19 17:17:12 +0600

    First commit from NetBeans

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author HP
+ */
+import java.util.Scanner;
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is: " + name);
+        scanner.close();
+    }
+}
+    
